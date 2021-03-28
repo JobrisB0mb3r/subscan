ba52fdae3d6c53544eedcd05f72cf4b81cda5c6a
JobrisB0mb3r <jobrisbomber@gmail.com>
2021-03-28 12:01:35 +0800

    Memperbaiki README dan setup.py

---
+++ README.md
@@ -0,0 +1,5 @@
+# Subscan
+## Table of Contents
+## Description
+Subscan is a subdomains scanning tools.
+## Usage Example
--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="subscan",
+    url="https://github.com/JobrisB0mb3r/subscan",
     version="1.0",
     python_requires=">=3.8",
     description="Subdomains scanning tools"
